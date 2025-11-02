8410dfbba49ed910a62c6051f7e250c10f452206
Holger Schmermbeck <holger@secpal.app>
2025-11-03 00:56:25 +0100

    style: fix prettier formatting

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -14,7 +14,7 @@ describe("App", () => {
   it("renders main content", () => {
     render(<App />);
     expect(
-      screen.getByText(/SecPal - a guard's best friend/i),
+      screen.getByText(/SecPal - a guard's best friend/i)
     ).toBeInTheDocument();
   });
 
